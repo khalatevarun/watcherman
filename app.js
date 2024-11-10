9e433550baa26272b8182e2297b9dab3088a7edb
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-11-10 00:25:07 -0600

    complete watchman setup

---
+++ app.js
@@ -0,0 +1,17 @@
+import WatchMan from './lib/watchman.js';
+const websites = [
+{
+    url: 'http://crushit-compiler.herokuapp.com',
+    timeout: 0.25
+}];
+
+const monitors = [];
+
+websites.forEach(function (website) {
+    const monitor = new WatchMan({
+        website: website.url,
+        timeout: website.timeout
+    });
+    monitors.push(monitor);
+});
+
