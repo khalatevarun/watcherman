26e3406d3770d8b5fe042afeca997459c7d83378
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-11-21 11:23:27 -0600

    update base url on frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,7 +9,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE ?? ''
 );
 
-const BASE_URL = 'https://watchman-beta.vercel.app/'
+const BASE_URL = 'https://watchman-backend.vercel.app/'
 
 interface Website {
   id: number;
